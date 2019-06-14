c463617aa804f84001e7af1a3111117993187577
kakastg <kakastg9@gmail.com>
2019-06-14 10:53:31 +0200

    Hello World program just to fill the folder with something

---
+++ Scripts/test_script.cs
@@ -0,0 +1,11 @@
+using System;
+namespace test_script{
+	class Hello{
+		public static void Main(){
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
+	}
+}
+
